ee8cade0ef02feeebb1ca352e5bfc423bbc7dd52
Cankutay Katkat <katkatcankutay@gmail.com>
2022-09-13 20:22:35 +0200

    game

---
+++ game.js
@@ -0,0 +1,55 @@
+//* =====================================
+//*  GUESS MY NUMBER
+//*  Game Logic
+//*======================================
+
+//? 1-100 arasinda rasgele bir sayi tut.
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
+
+//? Variables
+let score = 10;
+let topScore = 0;
+
+//* CheckBtn basildiginda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+  const guessInput = Number(document.querySelector(".guess-input").value);
+  const msg = document.querySelector(".msg");
+  const body = document.querySelector("body");
+
+  //? eger input girilmediyse Kullaniciya uyari ver.
+  if (!guessInput) {
+    msg.innerText = "Please enter a number";
+    //! eger rasgele == input.value
+  } else if (randomNumber === guessInput) {
+    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
+    body.className = "bg-success";
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+    document.querySelector(".secret-number").textContent = randomNumber;
+  } else {
+    score--;
+
+    guessInput > randomNumber
+      ? (msg.innerText = "DECREASE")
+      : (msg.innerText = "INCREASE");
+  }
+});
+
+//? eger score > topScore
+//?     topScore = score
+//? secret_number = gorunur.
+
+//! değilse
+//! eger score > 0
+//!   score = score -1
+//?   eğer rasgele < input.value
+//?     AZALT
+//?   degilse
+//?     ARTTIR
+//! degise
+//? Uzgunuz kaybetiniz.
+
+//* againBtn basildiginda kontrolleri yap
